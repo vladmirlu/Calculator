c27da2a56c6441d854ffe1e950c51d8982f40c6e
Volodydyr <lubikvova@gmail.com>
2018-11-01 02:32:21 +0200

    update01.11.2

---
+++ src/com/math/calculator/calculation/MathDecorator.java
@@ -0,0 +1,31 @@
+package com.math.calculator.calculation;
+
+public class MathDecorator {
+
+    private final Validator validator;
+
+    public MathDecorator(Validator validator) {
+        this.validator = validator;
+    }
+
+    public String decorate(String expression) throws Exception {
+
+        StringBuilder stack = new StringBuilder();
+        MathElementsCollector formatter = new MathElementsCollector(stack);
+        StringBuilder elements = new StringBuilder();
+
+        for (int i = 0; i < expression.length(); i++) {
+            String element = Character.toString(expression.charAt(i));
+            if (validator.isOperator(element)) {
+                elements = formatter.orderOperators(element, validator, elements);
+            } else if (validator.isOpenBracket(element)) {
+                stack.append(element);
+            } else if (validator.isCloseBracket(element)) {
+                elements = formatter.orderElementsInBrackets(validator, elements);
+            } else {
+                elements.append(element); // Если символ не оператор - добавляем в выходную последовательность
+            }
+        }
+        return formatter.getOrderedString(elements);
+    }
+}
--- src/com/math/calculator/calculation/Service.java
+++ src/com/math/calculator/calculation/Service.java
@@ -16,12 +16,12 @@ public class Service {
 
     private final Validator validator = new Validator();
 
-    private final Decorator decorator = new Decorator(validator);
+    private final MathDecorator mathDecorator = new MathDecorator(validator);
 
     private final HistoryCreator historyCreator = new HistoryCreator(users);
 
     public String calculate(String expression, String username) throws  Exception{
-        expression = decorator.decorate(expression);
+        expression = mathDecorator.decorate(expression);
         Double result = getCalcResult(expression);
         historyCreator.createNewNote(username, expression, result);
         return result.toString();
@@ -29,23 +29,23 @@ public class Service {
 
     private Double getCalcResult(String expression) throws WrongDataQuantityException, OperatorNotFoundException {
 
-        Deque<Double> stack = new ArrayDeque<>();
+        Deque<Double> numbers = new ArrayDeque<>();
         StringTokenizer tokenizer = new StringTokenizer(expression);
         while (tokenizer.hasMoreTokens()) {
-               String temp = tokenizer.nextToken().trim();
-                if (temp.length() == 1 && validator.isOperator(Character.toString(temp.charAt(0)))) {
-                    Operator operator = validator.getOperator(Character.toString(temp.charAt(0)));
-                    if (stack.size() < 2) {
+               String nextToken = tokenizer.nextToken().trim();
+                if (nextToken.length() == 1 && validator.isOperator(Character.toString(nextToken.charAt(0)))) {
+                    if (numbers.size() < 2) {
                         throw new WrongDataQuantityException();
                     }
-                    double number = stack.pop();
-                    stack.push(operator.apply(stack.pop(), number));
+                    Operator operator = validator.getOperator(Character.toString(nextToken.charAt(0)));
+                    double number = numbers.pop();
+                    numbers.push(operator.apply(numbers.pop(), number));
                 } else {
-                    stack.push(Double.parseDouble(temp));
+                    numbers.push(Double.parseDouble(nextToken));
                 }
         }
-        if (stack.size() > 1) throw new WrongDataQuantityException();
-        return stack.pop();
+        if (numbers.size() > 1) throw new WrongDataQuantityException();
+        return numbers.pop();
     }
 
     public List<User> getUsers() {
